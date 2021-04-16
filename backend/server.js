5624035320b4091f7ed97b7727926ac00652260b
Majd Lefi <majd.lefi@esprit.tn>
2021-04-16 02:06:13 +0100

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,34 @@
-const express = require("express")
-const app = express()
-const authRouter = require('./routes/auth')
-const bootcampRoutes = require('./routes/bootcamp')
-const userRouter = require('./routes/user')
+const express = require("express");
+const app = express();
+const authRouter = require("./routes/auth");
+const bootcampRoutes = require("./routes/bootcamp");
+const userRouter = require("./routes/user");
 
-//Middleware 
-app.use(express.json())
+//Middleware
+app.use(express.json());
 
 //connect DB
-const connectDB = require('./config/connectDB')
-connectDB()
+const connectDB = require("./config/connectDB");
+connectDB();
 
 //use the routes
 
+app.use("/api/auth", authRouter);
+app.use("/", bootcampRoutes);
+app.use("/", userRouter);
 
-app.use('/api/auth',authRouter);
-app.use('/',bootcampRoutes);
-app.use('/',userRouter);
+const port = process.env.PORT || 2000;
 
-const port = process.env.PORT || 2000
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+//heroku part
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
 }
 
 app.listen(port, (error) =>
-    error
-        ? console.log(error)
-        : console.log(`The server is running on port ${port}`)
-)
+  error
+    ? console.log(error)
+    : console.log(`The server is running on port ${port}`)
+);
