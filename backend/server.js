b286271b35470c378a174051906aece6061b75ec
Majd Lefi <majd.lefi@esprit.tn>
2021-04-16 03:40:26 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,8 @@ app.use("/", userRouter);
 const port = process.env.PORT || 2000;
 
 //heroku part
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/buid")));
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
