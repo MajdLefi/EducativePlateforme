a50b522befcbabac42b108f56f922b1469766395
Majd Lefi <majd.lefi@esprit.tn>
2021-04-16 03:20:05 +0100

    deployment heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const authRouter = require("./routes/auth");
 const bootcampRoutes = require("./routes/bootcamp");
@@ -21,9 +22,13 @@ const port = process.env.PORT || 2000;
 
 //heroku part
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/buid")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Apo running");
   });
 }
 
