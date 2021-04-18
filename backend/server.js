3109457c798d4cc22cac7698898d7294f9876cf7
Majd Lefi <majd.lefi@esprit.tn>
2021-04-18 03:24:53 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -21,14 +21,12 @@ app.use("/", userRouter);
 const port = process.env.PORT || 2000;
 
 //heroku part
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static("client/build"));
+  
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("Apo running");
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
